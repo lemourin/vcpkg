803992922601700b4ac32722e20f8938e92cbb33
Paweł Wegner <pawel.wegner95@gmail.com>
2023-10-22 13:09:01 +0200

    [meson] update to 1.2.1

--- ports/vcpkg-tool-meson/portfile.cmake
+++ ports/vcpkg-tool-meson/portfile.cmake
@@ -5,17 +5,17 @@
 set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
 
 set(program MESON)
-set(program_version 0.63.0)
+set(program_version 1.2.1)
 set(program_name meson)
 set(search_names meson meson.py)
 set(interpreter PYTHON3)
 set(apt_package_name "meson")
 set(brew_package_name "meson")
-set(ref bb91cea0d66d8d036063dedec1f194d663399cdf)
+set(ref 84e437179da05db10aa2457322c54f4ec8be61f1)
 set(paths_to_search "${CURRENT_PACKAGES_DIR}/tools/meson")
 set(download_urls "https://github.com/mesonbuild/meson/archive/${ref}.tar.gz")
 set(download_filename "meson-${ref}.tar.gz")
-set(download_sha512 e5888eb35dd4ab5fc0a16143cfbb5a7849f6d705e211a80baf0a8b753e2cf877a4587860a79cad129ec5f3474c12a73558ffe66439b1633d80b8044eceaff2da)
+set(download_sha512 5f75af79bd6e05461a7e426487c77b5600bc94eed3184a26d4deed1ef960e28b889208e1b039e67606b8290321e0b5184227a476a25dd72a4f99c41ecfb37b8c)
 set(supported_on_unix ON)
 set(version_command --version)
 set(extra_search_args EXACT_VERSION_MATCH)
@@ -44,11 +44,6 @@ if(NOT "${program}")
                         WORKING_DIRECTORY "${CURRENT_PACKAGES_DIR}/tools"
                     )
     file(RENAME "${CURRENT_PACKAGES_DIR}/tools/meson-${ref}" "${CURRENT_PACKAGES_DIR}/tools/meson")
-    z_vcpkg_apply_patches(
-        SOURCE_PATH "${CURRENT_PACKAGES_DIR}"
-        PATCHES
-            meson-intl.patch
-    )
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/tools/meson/test cases")
     configure_file("${CMAKE_CURRENT_LIST_DIR}/vcpkg-port-config.cmake" "${CURRENT_PACKAGES_DIR}/share/${PORT}/vcpkg-port-config.cmake" @ONLY)
 endif()
